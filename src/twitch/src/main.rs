47e0cc88c0faafa21ac4a5cb3cbb513dc61a77ec
Shizcow <pohl.devin@gmail.com>
2021-03-03 12:02:59 -0700

    Cleaned up some stuff

--- src/twitch/src/main.rs
+++ src/twitch/src/main.rs
@@ -20,27 +20,6 @@ fn main() -> anyhow::Result<()> {
         )
         .get_matches();
 
-    /* command overview:
-    -f, forward: move forward
-    -b, backward: move backward
-    -l, left: turn left
-    -r, right: turn right
-    -u, up: look up
-    -d, down: look down
-    -say: emit a phrase from the built in speaker
-
-    Always available:
-    -help: display commands
-    -info: bot state
-    -up, uptime: uptime
-    -source: source code
-
-    Additionally, admin commands are available:
-    -stop: stop accepting input from non-admins
-    -start: begin accepting input from non-admins
-    -quit: kill the bot
-    */
-
     let bot = ChatBot::new_from_file(matches.value_of("config").unwrap())?.with_command(
         "help",
         |chat, _, _| {
@@ -73,27 +52,6 @@ fn main() -> anyhow::Result<()> {
 	    }
         },
     );
-    // .with_command("!info", |chat: Chat| {
-    //     let output = format!("Source Code: https://github.com/Shizcow/BooBot");
-    //     chat.writer.say(chat.msg, &output).unwrap();
-    // })
-    // .with_command("!hello", |chat: Chat| {
-    //     let output = format!("hello {}!", chat.msg.name());
-    //     // We can 'reply' to this message using a writer + our output message
-    //     chat.writer.reply(chat.msg, &output).unwrap();
-    // })
-    // .with_command("!uptime", move |chat: Chat| {
-    //     let output = format!("its been running for {:.2?}", start.elapsed());
-    //     // We can send a message back (without quoting the sender) using a writer + our output message
-    //     chat.writer.say(chat.msg, &output).unwrap();
-    // })
-    // .with_command("!quit", move |chat: Chat| {
-    //     // because we're using sync stuff, turn async into sync with smol!
-    //     smol::block_on(async move {
-    //         // calling this will cause read_message() to eventually return Status::Quit
-    //         chat.quit.notify().await
-    //     });
-    // });
 
     smol::block_on(async move { bot.run().await })
 }
